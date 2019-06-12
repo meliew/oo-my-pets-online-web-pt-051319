c6b324cce6cea04780595feb89eb5a6c71dfef91
Melissa Williams <melissa@brandmeetscopy.com>
2019-06-12 05:20:30 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -36,27 +36,27 @@ def self.all
   end
 
   def buy_cat(cat)
-    @@pets[:cats] << Cat.new(cat)
+    pets[:cats] << Cat.new(cat)
   end
 
   def buy_dog(dog)
-    @@pets[:dogs] << Dog.new(dog)
+  pets[:dogs] << Dog.new(dog)
   end
 
   def walk_dogs
-    @@pets[:dogs].each do |dog|
+    pets[:dogs].each do |dog|
       dog.mood = "happy"
     end
   end
 
   def play_with_cats
-    @@pets[:cats].each do |cat|
+    pets[:cats].each do |cat|
       cat.mood = "happy"
     end
   end
 
   def feed_fish
-    @@pets[:fishes].each do |fish|
+  pets[:fishes].each do |fish|
       fish.mood = "happy"
     end
   end
