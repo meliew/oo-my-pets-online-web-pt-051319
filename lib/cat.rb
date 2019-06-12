e90a70525ff3725259cca70ff14357828d810e81
Melissa Williams <melissa@brandmeetscopy.com>
2019-06-12 04:34:56 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -3,9 +3,9 @@ class Cat
   attr_reader :name
   attr_accessor :mood
 
-def initialize(name, mood)
+def initialize(name, mood= "nervous")
   @name = name
-  @mood = "nervous"
+  @mood = mood
 end
 
 def mood
